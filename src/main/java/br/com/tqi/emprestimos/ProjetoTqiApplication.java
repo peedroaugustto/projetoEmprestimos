6db79321a715263c73ac1fb00e5a8ee2ed780b8e
Pedro Augusto <peedroaugustto@hotmail.com>
2020-08-28 22:23:31 -0300

    Criação do projeto

---
+++ src/main/java/br/com/tqi/emprestimos/ProjetoTqiApplication.java
@@ -0,0 +1,13 @@
+package br.com.tqi.emprestimos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTqiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTqiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tqi/emprestimos/ProjetoTqiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tqi.emprestimos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTqiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
